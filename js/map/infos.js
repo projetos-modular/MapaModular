f231ac9c82623b24ee57f92e85cd925c2ab10562
Camila Becker <camila.becker01@gmail.com>
2020-04-17 14:57:24 -0300

    Atualizado infos

--- js/map/infos.js
+++ js/map/infos.js
@@ -10,7 +10,7 @@
     info.update = function (props) {
 
       this._div.innerHTML = '<h4>Dados da cidade: </h4>' + (props ?
-        '<b>' + props.nome + '</b><br/><br/>' + 'População: ' + props.populacao.toLocaleString('pt-BR') + '<br/><br/>' + 'PIB: R$ ' + props.pib.toLocaleString('pt-BR') + '<br/><br/>' + 'Região: ' + props.regiao + '<br/><br/>' + 'Valor NF: R$ ' + props.valorNF.toLocaleString('pt-BR') : 'Passe o mouse sobre a cidade');
+        '<b>' + props.nome + '</b><br/><br/>' + 'Frete: R$ ' + props.frete.toLocaleString('pt-BR') + '<br/><br/>' + 'Custo Transferência: R$ ' + props.custoTransferencia.toLocaleString('pt-BR') + '<br/><br/>' + 'Margem Bruta: R$ ' + props.margemBruta.toLocaleString('pt-BR') + '<br/><br/>' + 'Percentual Margem Bruta: ' + props.margemBrutaPerc + '%' + '<br/><br/>' + 'Valor NF: R$ ' + props.valorNF.toLocaleString('pt-BR') + '<br/><br/>' + 'Parceiro: ' + props.parceiro + '<br/><br/>' + 'Região: ' + props.regiao + '<br/><br/>' + 'Unidade: ' + props.unidade + props.regiao + '<br/><br/>' + 'Peso: ' + props.peso.toLocaleString('pt-BR') + ' Kg' + '<br/><br/>' + 'Cubagem: ' + props.cubagem.toLocaleString('pt-BR') + '<br/><br/>' + 'PIB: R$ ' + props.pib.toLocaleString('pt-BR') + '<br/><br/>' + 'PIB per Capita: R$ ' + props.pibPerCapita.toLocaleString('pt-BR') + '<br/><br/>' + 'População: ' + props.populacao.toLocaleString('pt-BR') + '<br/><br/>'+ 'ID: ' + props.id : 'Passe o mouse sobre a cidade');
       };
 
     info.addTo(map);
